5986ff01ac28bad651990cb881351874be1ef3e1
skripka_sv <Centauris@yandex.ru>
2020-01-20 12:07:50 +0300

    ver

---
+++ week2-2.py
@@ -0,0 +1,12 @@
+A = int(input())
+B = int(input())
+C = int(input())
+if A >= B:
+    if A >= C:
+        print(A)
+elif B >= A:
+    if B >= C:
+        print(B)
+elif C >= A:
+    if C >= B:
+        print(C)
