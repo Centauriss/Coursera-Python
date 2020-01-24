9879cf9799298b1a631b00b82a2b852a251e5034
skripka_sv <Centauris@yandex.ru>
2020-01-24 11:44:11 +0300

    ver

---
+++ test_week2.py
@@ -0,0 +1,3 @@
+x1 = int(input())
+x2 = int(input())
+print(abs(x1 - x2) % 2)
---
+++ week2-6.py
@@ -0,0 +1,8 @@
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if (abs(x1 - x2) % 2 == 0) and (abs(y1 - y2) % 2 == 0) or ((x1 == y1) and (x2 == y2)):
+    print('YES')
+elif:
+    print('NO')
