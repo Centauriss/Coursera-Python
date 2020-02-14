9fbdeaf09212bf68c216cc0be9651be7a860908e
skripka_sv <Centauris@yandex.ru>
2020-02-14 17:06:07 +0300

    ver

--- week2-6.py
+++ week2-6.py
@@ -4,5 +4,7 @@ x2 = int(input())
 y2 = int(input())
 if (abs(x1 - x2) % 2 == 0) and (abs(y1 - y2) % 2 == 0) or ((x1 == y1) and (x2 == y2)):
     print('YES')
+elif (abs(x1 - x2) % 2 == 1) and (abs(y1 - y2) % 2 == 1) or ((x1 == y1) and (x2 == y2)):
+    print('YES')
 else:
     print('NO')
