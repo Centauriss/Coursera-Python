d7ce91603d8f9ed21521b06c213accd548760d9e
skripka_sv <Centauris@yandex.ru>
2020-05-14 15:47:53 +0300

    ver

--- week2-tip-trieugholnika.py
+++ week2-tip-trieugholnika.py
@@ -4,12 +4,13 @@ c = int(input())
 A = a ** 2
 B = b ** 2
 C = c ** 2
+#  a + b > c or a + c > b or b + c > a:
 if a > b and a > c and A == B + C:
-    print('rectangular')
+    print('rectangular')    # Прямоугольный
 elif a > b and a > c and A < B + C:
-    print('acute')
+    print('acute')  # Острый
 elif a > b and a > c and A > B + C:
-    print('obtuse')
+    print('obtuse')  # Тупой
 elif b > a and b > c and B == A + C:
     print('rectangular')
 elif b > a and b > c and B < A + C:
