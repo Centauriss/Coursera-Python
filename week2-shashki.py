ef0d0f7693e782275d35cbb3ab737a33518d33c5
skripka_sv <Centauris@yandex.ru>
2020-05-15 17:19:38 +0300

    Revert "ver"
    This reverts commit d7ce91603d8f9ed21521b06c213accd548760d9e.
    
    # Conflicts:
    #	week2-tip-trieugholnika.py

--- week2-shashki.py
+++ week2-shashki.py
@@ -3,15 +3,12 @@ y1 = int(input())
 x2 = int(input())
 y2 = int(input())
 if y1 < y2:  # Движение вверх по полю
-    if x1 == y1 and x2 == y2:  # Движение по диагоналям
+#  проверка диагонали
+    if (x1 == y1 and x2 == y2) or (abs(x2-x1) == abs(y2-y1)):
         print('YES')
-#   elif x1 == x2 and (y2 - y1 + 2) % 2 == 1:  # Движение по вертикальному краю
-#       print('YES2')
-#   elif (x2 - x1) % 2 == 0 and y1 == y2:
-#      print('YES3')
     elif (x2 - x1) % 2 == 0 and (y2 - y1) % 2 == 0:
         print('YES')
     else:
         print('NO')
 else:
-    print("NO")
+    print('NO')
