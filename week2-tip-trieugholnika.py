7cbc03520526ea601632e8fbe587125c26611d6c
skripka_sv <Centauris@yandex.ru>
2020-05-21 08:44:02 +0300

    Рабочая версия. Сдалась. Тест 100/100.
    Revert "ver"
    
    This reverts commit d7ce91603d8f9ed21521b06c213accd548760d9e.
    
    # Conflicts:
    #	week2-tip-trieugholnika.py

--- week2-tip-trieugholnika.py
+++ week2-tip-trieugholnika.py
@@ -6,21 +6,21 @@ B = b ** 2
 C = c ** 2
 if a > b and a > c and A == B + C:
     print('rectangular')    # Прямоугольный
-elif a > b and a > c and A < B + C:
+elif a >= b and a >= c and A < B + C:
     print('acute')  # Острый
-elif a > b and a > c and A > B + C and a < b + c:
+elif a >= b and a >= c and A > B + C and a < b + c:
     print('obtuse')  # Тупой
-elif b > a and b > c and B == A + C:
+elif b >= a and b >= c and B == A + C:
     print('rectangular')
-elif b > a and b > c and B < A + C:
+elif b >= a and b >= c and B < A + C:
     print('acute')
-elif b > a and b > c and B > A + C and b < a + c:
+elif b >= a and b >= c and B > A + C and b < a + c:
     print('obtuse')
-elif c > a and c > b and C == B + A:
+elif c >= a and c >= b and C == B + A:
     print('rectangular')
-elif c > a and c > b and C < B + A:
+elif c >= a and c >= b and C < B + A:
     print('acute')
-elif c > a and c > b and C > B + A and c < a + b:
+elif c >= a and c >= b and C > B + A and c < a + b:
         print('obtuse')
 else:
     print('impossible')
