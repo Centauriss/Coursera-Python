724f1563ee81f4cd478e429c8bd2dd5d4cff1ab6
skripka_sv <Centauris@yandex.ru>
2020-05-22 14:59:40 +0300

    Рабочая версия. Сдалась. Тест 100/100.
    Revert "ver"
    
    This reverts commit d7ce91603d8f9ed21521b06c213accd548760d9e.
    
    # Conflicts:
    #	week2-tip-trieugholnika.py

---
+++ week2-uznik-zamka-if.py
@@ -0,0 +1,5 @@
+a, b, c, d, e = int(input()), int(input()), int(input()), int(input()), int(input())
+if (d >= a or d >= b or d >= c) and (e >= a or e >= b or e >= c):
+    print('YES')
+else:
+    print('NO')
