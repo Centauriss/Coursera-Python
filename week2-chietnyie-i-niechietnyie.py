ff196088c24165da53598abc27a48b044979b9e9
skripka_sv <Centauris@yandex.ru>
2020-05-22 17:01:23 +0300

    Рабочая версия. Сдалась. Тест 100/100.
    Revert "ver"
    
    This reverts commit d7ce91603d8f9ed21521b06c213accd548760d9e.
    
    # Conflicts:
    #	week2-tip-trieugholnika.py

--- week2-chietnyie-i-niechietnyie.py
+++ week2-chietnyie-i-niechietnyie.py
@@ -4,13 +4,7 @@ c = int(input())
 A = abs(a)
 B = abs(b)
 C = abs(c)
-if A <= 10000 and B <= 1000 and C <= 1000:
-    if A % 2 == 0 or B % 2 == 0 or C % 2 == 0:
-        if A % 2 == 1 or B % 2 == 1 or C % 2 == 1:
-            print('YES')
-        else:
-            print('NO')
-    else:
-        print('NO')
-else:
+if A % 2 == B % 2 == C % 2:
     print('NO')
+else:
+    print('YES')
